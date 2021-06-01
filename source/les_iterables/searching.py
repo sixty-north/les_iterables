6afaf215693f09ac577ed4fdf8beb5ad3b5dc134
Robert Smallshire <rob@sixty-north.com>
2021-06-01 13:21:37 +0200

    Adds missing documentation

--- source/les_iterables/searching.py
+++ source/les_iterables/searching.py
@@ -2,11 +2,36 @@ from les_iterables import retain_if
 from les_iterables.selecting import element_at
 
 
-def nth_matching(iterable, predicate, index):
-    return element_at(retain_if(iterable, predicate), index)
+def nth_matching(iterable, predicate, n):
+    """The nth item matching a predicate.
+
+    Args:
+        iterable: An iterable series of items to be searched.
+        predicate: A callable to which each item will be passed in turn.
+        n: A zero-based index.
+
+    Returns:
+        The nth item for which the predicate returns True.
+
+    Raises:
+        ValueError: If there are no matching items.
+    """
+    return element_at(retain_if(iterable, predicate), n)
 
 
 def first_matching(iterable, predicate):
+    """The first item matching a predicate.
+
+    Args:
+        iterable: An iterable series of items to be searched.
+        predicate: A callable to which each item will be passed in turn.
+
+    Returns:
+        The first item for which the predicate returns True.
+
+    Raises:
+        ValueError: If there are no matching items.
+    """
     for item in iterable:
         if predicate(item):
             return item
