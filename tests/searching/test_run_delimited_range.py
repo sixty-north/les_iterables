fef2cea0abcdcffa2a7d9ab4d240f06c9e338dae
Ekaterina Nikonova <ekaterina@sixty-north.com>
2023-08-04 11:37:27 +0200

    Break down a test

--- tests/searching/test_run_delimited_range.py
+++ tests/searching/test_run_delimited_range.py
@@ -20,10 +20,21 @@ def test_iterable_with_three_unequal_elements_produces_1_2_range():
     assert run_delimited_range('xyz') == range(1, 2)
 
 
-def test_run_delimited_range_works():
-    # TODO: break down this test
-    assert run_delimited_range([1, 1, 1, 3, 3, 8, 15, 15, 15, 8, 18, 18]) == range(3, 10)
-    assert run_delimited_range([-5, -1, 3, -8, 14, -2, -1], lambda a, b: a * b >= 0) == range(2, 5)
-    assert run_delimited_range(["0", "1", "2", 3, 4, "5"], lambda a, b: type(a) == type(b)) == range(3, 5)
-    assert run_delimited_range(["0", "1", "2", 3, 4, "5", 6, 7], lambda a, b: type(a) == type(b)) == range(3, 6)
+def test_run_delimited_range_works_for_a_string():
     assert run_delimited_range('xxxxxxxxxxyyyyy') == range(10, 10)
+
+
+def test_run_delimited_range_works_for_a_list_of_integers():
+    assert run_delimited_range([1, 1, 1, 3, 3, 8, 15, 15, 15, 8, 18, 18]) == range(3, 10)
+
+
+def test_run_delimited_range_works_with_a_sign_comparator():
+    assert run_delimited_range([-5, -1, 3, -8, 14, -2, -1], lambda a, b: a * b >= 0) == range(2, 5)
+
+
+def test_run_delimited_range_works_with_a_type_comparator_for_the_same_delimiter_type():
+    assert run_delimited_range(["0", "1", "2", 3, 4, "5"], lambda a, b: type(a) == type(b)) == range(3, 5)
+
+
+def test_run_delimited_range_works_with_a_type_for_different_delimiter_types():
+    assert run_delimited_range(["0", "1", "2", 3, 4, "5", 6, 7], lambda a, b: type(a) == type(b)) == range(3, 6)
